edb32e0e6f277b6d6563cc47afee6f1b221778f3
bastafidli <bastafidli@opensubsystems.org>
2013-05-16 01:10:35 -0400

    Initial revision of ParametrizedContext class.

---
+++ sources/code/java/org/opensubsystems/pattern/parameter/data/ParametrizedContext.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2003 - 2013 OpenSubsystems.com/net/org and its owners. All rights reserved.
+ * 
+ * This file is part of OpenSubsystems.
+ *
+ * OpenSubsystems is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.opensubsystems.pattern.parameter.data;
+
+import java.util.Stack;
+
+/**
+ * Interface representing context within which some processing is being executed
+ * that can be configured using set of parameters.
+ *
+ * @author bastafidli
+ */
+public interface ParametrizedContext 
+{
+   /**
+    * Get the collection of objects that can parametrize processing using this 
+    * context.
+    */
+   Stack<ParametrizedObject> getParamContext();
+
+   /**
+    * Add new parametrized object to the context used for some kind of processing.
+    * Parameters defined in the added object will take precedence over any 
+    * previously added parameters.
+    * 
+    * @param newContext - new parametrized object for some kind of processing.
+    */
+   void pushParamContext(
+      ParametrizedObject newContext
+   );
+   
+   /**
+    * Remove the last added parametrized object from the context used for some 
+    * kind of processing. If parameters from the removed object redefined some 
+    * parameters from earlier added objects, those parameters will not be 
+    * restored.
+    * 
+    * @return ParametrizedObject - previously added parametrized object 
+    */
+   ParametrizedObject popParamContext();
+}
