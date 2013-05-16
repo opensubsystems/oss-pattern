ee0883ad717cb5db9fb391b768899244f2c7b4af
bastafidli <bastafidli@opensubsystems.org>
2013-05-16 00:22:14 -0400

    Initial revision of ParametrizedObject class.

---
+++ sources/code/java/org/opensubsystems/pattern/parameter/data/ParametrizedObject.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2003 - 2013 OpenSubsystems.com/net/org and its owners. All rights reserved.
+ * 
+ * This file is part of OpenSubsystems.
+ *
+ * OpenSubsystems is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.opensubsystems.pattern.parameter.data;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Interface representing object with set of parameters that can be used to 
+ * further characterize the behavior of the object.
+ *
+ * @author bastafidli
+ */
+public interface ParametrizedObject 
+{
+    /**
+     * Get parameters, values of which are used to further characterize 
+     * the behavior of the object.
+     * 
+     * @return Map<String, Parameter> - key is the parameter name, value is the 
+     *                                  parameter itself. Can be null.
+     */
+    Map<String, Parameter>getParametersByName();
+
+   /**
+    * Get collection of the parameters, values of which are used to further 
+    * characterize the behavior of the object.
+    * 
+    * @return Collection<Parameter> - collection of parameters. Can be null.
+    */
+   Collection<Parameter> getParameters();
+}
